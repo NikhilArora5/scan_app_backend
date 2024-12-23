bc9fd3bea4e64a537701c349a4ba89b68e8309c7
Nikhil <aroranikhil8184@gmail.com>
2024-12-23 17:41:30 +0530

    netlify

--- functions/api.js
+++ functions/api.js
@@ -13,26 +13,30 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT=process.env.PORT || 8082
 
-app.get("/",(req,res)=>{
-    res.send("Hello Deployed")
-  })
+// app.get("/",(req,res)=>{
+//     res.send("Hello Deployed")
+//   })
 
+const router=express.Router()
 
-const TipsRouter=require("./src/Modules/Tips/index")
+router.get("/",(req,res)=>{
+       res.send("Hello Deployed")
+     })
+// const TipsRouter=require("./src/Modules/Tips/index")
 
-app.use("/tips/",TipsRouter)
-app.use("/api/", res.send("Hello Deployedapai "))
+// app.use("/tips/",TipsRouter)
+// app.use("/api/", res.send("Hello Deployedapai "))
   
-  app.listen(PORT,(err)=>{
-      if(err){
-          console.log("server error")
-      }else{
+  // app.listen(PORT,(err)=>{
+  //     if(err){
+  //         console.log("server error")
+  //     }else{
   
-          console.log(`Server Running  ✔️   on http://localhost:${PORT}/`)
-      }
-  })
+  //         console.log(`Server Running  ✔️   on http://localhost:${PORT}/`)
+  //     }
+  // })
 
 
-app.use('/.netlify/functions/App', router);
-export const handler = serverless(app);
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app);
   
