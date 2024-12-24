f0d0383ce6a6015ff4bd01f907e76e57c71546ba
Nikhil <aroranikhil8184@gmail.com>
2024-12-24 11:35:10 +0530

    env variables

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     database: process.env.DB_NAME || 'database_development',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'postgres',
-    url:"postgresql://devnikhil585:7SJECtwfDv3Z@ep-quiet-wind-a5egf0y2.us-east-2.aws.neon.tech/next_sql?sslmode=require"
+    url:process.env.DB_URL
   },
   test: {
     username: process.env.DB_USER || 'postgres',
@@ -15,6 +15,7 @@ module.exports = {
     database: process.env.DB_NAME || 'database_test',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'postgres',
+    url:process.env.DB_URL
   },
   production: {
     username: process.env.DB_USER || 'postgres',
@@ -22,5 +23,6 @@ module.exports = {
     database: process.env.DB_NAME || 'database_production',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'postgres',
+    url:process.env.DB_URL
   },
 };
