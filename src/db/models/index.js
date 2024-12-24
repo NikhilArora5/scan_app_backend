78d7f517875d800ffc63311229db0e08f4733483
Nikhil <aroranikhil8184@gmail.com>
2024-12-24 11:37:59 +0530

    pg dialect module

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,11 +1,13 @@
 const { Sequelize } = require('sequelize');
 const config = require('../config/config');
+const pg =require("pg")
 
 const env = process.env.NODE_ENV || 'development';
 const dbConfig = config[env];
 
 const sequelize = new Sequelize(dbConfig.url, {
   dialect: dbConfig.dialect,
+  dialectModule:pg
 });
 
 const db = {};
