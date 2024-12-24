5ae05024d7bacb38dcdb13a3382809a266982a4b
Nikhil <aroranikhil8184@gmail.com>
2024-12-24 10:06:15 +0530

    netlify working

--- functions/api.js
+++ functions/api.js
@@ -18,23 +18,22 @@ const PORT=process.env.PORT || 8082
 //   })
 
 const router=express.Router()
+const TipsRouter=require("../src/Modules/Tips/index");
+const { getDailyTips } = require("../src/Modules/Tips/controller/TipsController");
+router.get("/tips",getDailyTips)
 
-router.get("/",(req,res)=>{
-       res.send("Hello Deployed")
-     })
-// const TipsRouter=require("./src/Modules/Tips/index")
 
 // app.use("/tips/",TipsRouter)
 // app.use("/api/", res.send("Hello Deployedapai "))
   
-  // app.listen(PORT,(err)=>{
-  //     if(err){
-  //         console.log("server error")
-  //     }else{
+  app.listen(PORT,(err)=>{
+      if(err){
+          console.log("server error")
+      }else{
   
-  //         console.log(`Server Running  ✔️   on http://localhost:${PORT}/`)
-  //     }
-  // })
+          console.log(`Server Running  ✔️   on http://localhost:${PORT}/`)
+      }
+  })
 
 
 app.use('/.netlify/functions/api', router);
